fe67b28c5f2f23eec4d6b3d47f7dde407cfe14f1
Mostafa Kenawey <ragab.mostafa@gmail.com>
2025-08-29 21:10:42 +0200

    docker and docker-compose

--- be/main.py
+++ be/main.py
@@ -9,7 +9,11 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=[
+        "http://localhost:5173",
+        "http://localhost:3000",
+        "http://frontend",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
